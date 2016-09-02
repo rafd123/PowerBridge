4b58634c5e02ebf8efda5058aeb489294933fc29
Rafael Goodman <rafael@getthefrog.com>
2016-09-02 16:40:42 -0400

    Fix #2: Use fully qualified task name when referencing the InvokePowerShell task with PowerBridge.targets

--- src/PowerBridge/NugetPackageFiles/tools/install.ps1
+++ src/PowerBridge/NugetPackageFiles/tools/install.ps1
@@ -14,11 +14,13 @@ $buildScriptsDestinationDirectoryPathExists = Test-Path (Join-Path $projectDirec
 $projectXml = [xml](Get-Content $projectFilePath)
 $namespaceManager = New-Object System.Xml.XmlNamespaceManager $projectXml.NameTable
 $namespaceManager.AddNamespace('msbuild', 'http://schemas.microsoft.com/developer/msbuild/2003')
-$invokePowerShellTaskExists = $projectXml.DocumentElement.SelectNodes('//msbuild:Target/msbuild:InvokePowerShell', $namespaceManager) | ForEach-Object { $_.GetEnumerator }
+$invokePowerShellTaskExists = $projectXml.DocumentElement.SelectNodes('//msbuild:Target/msbuild:PowerBridge.Tasks.InvokePowerShell.InvokePowerShell', $namespaceManager) | ForEach-Object { $_.GetEnumerator }
+$legacyInvokePowerShellTaskExists = $projectXml.DocumentElement.SelectNodes('//msbuild:Target/msbuild:InvokePowerShell', $namespaceManager) | ForEach-Object { $_.GetEnumerator }
 
 $shouldCreateDefaultBuildScripts = -not $buildScriptsProjectItemExists -and `
                                    -not $buildScriptsDestinationDirectoryPathExists -and `
-                                   -not $invokePowerShellTaskExists
+                                   -not $invokePowerShellTaskExists -and `
+                                   -not $legacyInvokePowerShellTaskExists
 
 if($shouldCreateDefaultBuildScripts)
 {    
